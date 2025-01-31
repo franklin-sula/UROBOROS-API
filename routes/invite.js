67425cba7462ce7170f34374f569940a510724b1
franklin-sula <franklin.sula09@gmail.com>
2025-01-31 09:55:07 +0800

    Update invite.js

--- routes/invite.js
+++ routes/invite.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { createInvite, acceptInvite } = require("../services/inviteService");
+const { createInvite } = require("../services/inviteService");
 const rateLimit = require("express-rate-limit");
 const validateInvite = require("../middleware/validateInvite");
 
